e280d3c5be85e21aa8ec3187b7bf6e28ab3f59ca
Дмитрий Александрович Худокормов <s0163526@edu.kubsu.ru>
2023-02-14 12:39:51 +0300

    123

---
+++ Monte-Carlo/Monte-Carlo/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monte_Carlo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Draw();
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl2";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(739, 468);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(821, 542);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
